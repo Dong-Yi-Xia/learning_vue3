6550d63b73981377de747c2e8d1f3bd68c188e24
Dong-Yi-Xia <dong7777xia@gmail.com>
2023-01-20 09:35:40 -0800

    dynamic components

--- src/components-usage/DynamicComponent/DynamicParent.vue
+++ src/components-usage/DynamicComponent/DynamicParent.vue
@@ -4,9 +4,21 @@
     <button @click="activeTab = 'TabB'">Tab B</button>
     <button @click="activeTab = 'TabC'">Tab C</button>
 
-    <TabA v-if="activeTab === 'TabA'" />
+    <!-- <TabA v-if="activeTab === 'TabA'" />
     <TabB v-if="activeTab === 'TabB'" />
-    <TabC v-if="activeTab === 'TabC'" />
+    <TabC v-if="activeTab === 'TabC'" /> -->
+
+    <h2>Dynamic Component</h2>
+    <!-- Dynamic Appoarch in rendering the register components -->
+    <!-- Use the component tag, use :is, and bind the register components, :is point to component name-->
+    <component :is="activeTab" />
+
+    <hr />
+    <h2>Cache The Component</h2>
+    <!-- keep-alive will cache the component and won't create an new instance, even when inactive state -->
+    <keep-alive>
+      <component :is="activeTab" />
+    </keep-alive>
   </div>
 </template>
 
--- src/components-usage/DynamicComponent/TabC.vue
+++ src/components-usage/DynamicComponent/TabC.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>Tab C Content</h1>
+    <input type="text" v-model="name" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'TabC',
+  data() {
+    return {
+      name: '',
+    };
+  },
 };
 </script>
 
